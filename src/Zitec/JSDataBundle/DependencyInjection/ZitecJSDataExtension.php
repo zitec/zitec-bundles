567cb4d0755275514da10e5bf7566e41cc61c942
Breta Ionut <breta.ionut@gmail.com>
2016-09-12 12:14:21 +0300

    ZitecJSDataBundle: Removed the configuration class.

--- src/Zitec/JSDataBundle/DependencyInjection/ZitecJSDataExtension.php
+++ src/Zitec/JSDataBundle/DependencyInjection/ZitecJSDataExtension.php
@@ -8,9 +8,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ * The ZitecJsDataBundle's service container extension.
  */
 class ZitecJSDataExtension extends Extension
 {
@@ -19,9 +17,6 @@ class ZitecJSDataExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
--- src/Zitec/JSDataBundle/ZitecJSDataBundle.php
+++ src/Zitec/JSDataBundle/ZitecJSDataBundle.php
@@ -4,6 +4,9 @@ namespace Zitec\JSDataBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * The ZitecJsDataBundle definition.
+ */
 class ZitecJSDataBundle extends Bundle
 {
 }
