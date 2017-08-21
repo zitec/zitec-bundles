6f52aff3bfc757d8ca2bca42201083788e9bd1de
Breta Ionut <breta.ionut@gmail.com>
2017-08-22 01:10:33 +0300

    Refactored the DataCollectorInterface a bit and brought it to PHP7 & Symfony standards

--- src/Zitec/JSDataBundle/DataCollector/DataCollectorInterface.php
+++ src/Zitec/JSDataBundle/DataCollector/DataCollectorInterface.php
@@ -7,7 +7,7 @@ namespace Zitec\JSDataBundle\DataCollector;
  *
  * A data collector is nothing more than an object whose role is to collect data from different components and expose
  * the final data set, which will get piped to the application's JS scripts. The data collector will output a single
- * value, as the intention is to have in JavaScript a single point of accessing the data received from the server.
+ * value, as the intention is to have in the client code a single point of accessing the data received from the server.
  *
  * The concrete data collectors will decide how to store and manage the collected data but they should expose a simple
  * API for:
@@ -16,7 +16,9 @@ namespace Zitec\JSDataBundle\DataCollector;
  *   A special exception should be thrown when that path is invalid;
  * - merging a data set to the existing data set. There will be cases when it will be much simpler to do that than
  *   adding the desired values;
- * - getting the collected data. The output of this method should be a value which can be JSON-encoded;
+ * - getting the collected data. The output of this method should be a value which can be JSON-encoded. Once the data
+ *   has been fetched from the collector, it should consider itself as "unloaded" and should no longer collect any other
+ *   data, throwing a special exception if a consumer demands this (via add or merge);
  */
 interface DataCollectorInterface
 {
@@ -24,14 +26,14 @@ interface DataCollectorInterface
      * Sets a value at the given path in the data set.
      *
      * @param string $path
-     * @param mixed $value
+     * @param mixed  $value
      *
      * @return $this
      *
-     * @throws InvalidPathException
-     * - when the path is invalid;
+     * @throws InvalidPathException       When the path is invalid.
+     * @throws UnloadedCollectorException If the collector was unloaded.
      */
-    public function add($path, $value);
+    public function add(string $path, $value);
 
     /**
      * Merges the given data set to the existing set.
@@ -39,14 +41,16 @@ interface DataCollectorInterface
      * @param mixed $data
      *
      * @return $this
+     *
+     * @throws InvalidArgumentException   When the data set cannot be merged.
+     * @throws UnloadedCollectorException If the collector was unloaded.
      */
     public function merge($data);
 
     /**
      * Fetches the collected data.
      *
-     * @return mixed
-     * - a value which can be JSON-encoded;
+     * @return mixed A value which can be JSON-encoded.
      */
     public function getAll();
 }
